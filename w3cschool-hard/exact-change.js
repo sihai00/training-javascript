069da250bfe7e8d81814e37795d3bbf24716406e
sihai <464990030@qq.com>
2017-07-05 09:54:50 +0800

    w3cschool hard No3 done

--- w3cschool-hard/exact-change.js
+++ w3cschool-hard/exact-change.js
@@ -2,6 +2,9 @@
 * 目标：收银系统
 * 解释：设计一个收银程序 checkCashRegister()，其把购买价格(price)作为第一个参数, 付款金额 (cash)作为第二个参数, 和收银机中零钱 (cid) 作为第三个参数，cid 是一个二维数组，存着当前可用的找零.
 * 例如：checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) 应该返回一个数组
+  思路：1.格式化cid，从面值大到小，例如100-20-10-5-0.25-0.1-0.05-0.01方便运算
+       2.比较找零和零钱，从大到小。如果面值比找零大，跳过，如果找零等于零钱，那么close，否则找零依次和零钱相减
+       3.最后判断如果剩余找零，那么Insufficient Funds，否则输出所负零钱
 * */
 
 function checkCashRegister(price, cash, cid) {
@@ -19,26 +22,30 @@ function checkCashRegister(price, cash, cid) {
   var price = Number(cash - price)
   var lave = []
   var result = []
+
   // 格式化cid
   sign.forEach(v => {
     price > v.value && lave.unshift(cid.find(k => k[0] === v.name))
   })
-  console.log(lave, 'lave')
+
   for(var v of lave){
-    console.log(price, v[1], (price - v[1]).toFixed(2), 'price')
-    
-    if (price > v[1]) {
-      console.log(1)
+    // console.log(price, v[1], (price - v[1]).toFixed(2), 'price')
+    var trading = sign.find(k => k.name === v[0])
+
+    if (price > v[1] && v[1] !== 0) {
       price = (price - v[1]).toFixed(2)
-      result.unshift(v)
+      result.push(v)
     }else if(price < v[1]){
-      console.log(2)
-      result.unshift([v[0], price])
-      if (Number.isInteger(v[1] - price)) {
-        price = 0
-        break
+      if (price < trading.value) {
+        continue
       }else{
-        price = (price - v[1]).toFixed(2)
+        var i = 0
+        while(price >= 0){
+          i += 1
+          price = (price - trading.value).toFixed(2)
+        }
+        price = (+price + trading.value).toFixed(2)
+        result.push([v[0], trading.value * (i - 1)])
       }
     }else if(price === v[1]){
       return 'Closed'
