4116c8f99853839790f3ac475711389859f6301c
sihai <464990030@qq.com>
2017-07-09 11:12:09 +0800

    w3cschool hard No6 done

--- w3cschool-hard/friendly-date-ranges.js
+++ w3cschool-hard/friendly-date-ranges.js
@@ -8,27 +8,36 @@
 // 日期转化
 function daySuffix(str){
   if (typeof str !== 'string') return
+  var pra = str[0] === '0' ? str.substr(-1) : str 
+  var res = ''
 
-  switch(str){
-    case str === '21' || str === '22' || str === '23':
-      return str + 'th'
-    case str.substr(-1) === '1'
-      return str + 'st'
-    case str.substr(-1) === '2'
-      return str + 'nd'
-    case str.substr(-1) === '3'
-      return str + 'rd'
-    default
-      return str + 'th'
+  switch(pra){
+    case '21' || '22' || '23':
+      res = pra + 'th'
+      break
+    case '1':
+      res = pra + 'st'
+      break
+    case '2':
+      res = pra + 'nd'
+      break
+    case '3':
+      res = pra + 'rd'
+      break
+    default:
+      res = pra + 'th'
   }
+  return res
 }
 
 // 月份转化
 function monthSuffix(str){
   if (typeof str !== 'string') return
+  console.log(str, 'monthSuffix')
+  var rule = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octobe', 'November', 'December']
+  var pra = str[0] === '0' ? str.substr(-1) : str
 
-  var rule = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octobe', 'November', 'December']
-  return rule[str]
+  return rule[pra]
 }
 
 function makeFriendlyDates(arr) {
@@ -36,16 +45,18 @@ function makeFriendlyDates(arr) {
 
   var arr0 = arr[0].split('-')
   var arr1 = arr[1].split('-')
+  var diff = (new Date(arr[1]).getTime() - new Date(arr[0]).getTime())/1000
+  let year = Math.floor(diff/3600/24/30/12);
+  let month = Math.floor(diff/3600/24/30);
 
-  return [``]
-
-  if (arr0[0] === arr1[0]) {
-    return [`${monthSuffix(arr0[1])} ${daySuffix(arr0[2])}`, `${arr0[1] === arr1[1] && monthSuffix(arr0[1])} ${daySuffix(arr0[2])}`]
+  if (arr[0] === arr[1]) {
+    return [`${monthSuffix(arr0[1])} ${daySuffix(arr0[2])}, ${arr0[0]}`]
   }else{
-    
+    return [
+      `${monthSuffix(arr0[1])} ${daySuffix(arr0[2])}${year !== 0 || (arr0[0] === arr1[0] && arr0[1] !== arr1[1])? ', ' + arr0[0] : ''}`, 
+      `${arr0[1] === arr1[1] && year === 0 ? '' : monthSuffix(arr1[1]) + ' '}${daySuffix(arr1[2])}${year === 0 || arr0[2] - arr1[2] === 1 ? '' : ', ' + arr1[0]}`
+    ]
   }
-
-  return arr
 }
 
 makeFriendlyDates(['2016-07-01', '2016-07-04'])
