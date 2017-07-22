6645161fb06d63f9cffc5704876fd0664c9ac31b
sihai <464990030@qq.com>
2017-07-23 00:21:55 +0800

    leetcode No6 done

--- leetcode/zigzag-conversion.js
+++ leetcode/zigzag-conversion.js
@@ -4,8 +4,34 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-  var result = []
-  for(var i = 0, len = s.length; v < len; i++){
-    
+  if (numRows <= 0) {
+    return ''
   }
+  if (numRows === 1 && numRows <= s.length) {
+    return s
+  }
+  var str = ''
+  var len = s.length
+
+  for(var i = 0; i < len && i < numRows; ++i){
+    var start = i
+    str += s.charAt(i)
+
+    for(var j = 1; start < len; ++j){
+      if (i === 0 || i === numRows - 1) {
+        start += 2 * numRows - 2
+      }else{
+        if (j % 2 === 1) {
+          start += 2 * (numRows - i - 1)
+        }else{
+          start += 2 * i
+        }
+      }
+      if (start < len) {
+        str += s.charAt(start)
+      }
+    }
+  }
+
+  return str
 };
