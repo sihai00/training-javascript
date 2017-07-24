5bd04fdcf0eec7dd328ed67ddac7d47da3473cc0
sihai <464990030@qq.com>
2017-07-24 09:25:55 +0800

    leetcode No7 done

--- leetcode/reverse-integer.js
+++ leetcode/reverse-integer.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+  var max = (1 << 30) * 2 - 1
+  var min =  - (1 << 30) * 2
+  var result = x.toString().split('').reverse()
+  
+  if (x < 0) result.pop()
+  result = x < 0 ? -result.join('') : +result.join('')
+
+  if (result > max || result < min) return 0
+
+  return result
 };
