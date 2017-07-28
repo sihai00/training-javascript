3e31bab5c75951d938c1e95e383a52bc68ae24fd
sihai <464990030@qq.com>
2017-07-28 23:46:37 +0800

    leetcode No20 done

--- leetcode/20-valid-parentheses.js
+++ leetcode/20-valid-parentheses.js
@@ -3,5 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  
+  var len = s.length
+  var result = []
+
+  for(var i = 0; i < len; i++){
+    if ('({['.indexOf(s[i]) !== -1) {
+      result.push(s[i])
+    }else{
+      if (result.length === 0) return false
+      var left = result.pop()
+      if ((left === '(' && s[i] === ')') || (left === '[' && s[i] === ']') || (left === '{' && s[i] === '}')) {
+        continue
+      }else{
+        return false
+      }
+    }
+  }
+  if (result.length === 0) {
+    return true
+  }else{
+    return false
+  }
 };
