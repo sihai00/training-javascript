410a05d733ee8387064a737bd9c78f8e3f8ade58
sihai <464990030@qq.com>
2017-07-29 00:06:04 +0800

    leetcode No20 more

--- leetcode/20-valid-parentheses.js
+++ leetcode/20-valid-parentheses.js
@@ -1,4 +1,5 @@
 /**
+ * 只能满足限定对应的符号[]、{}、()，利用栈的原理，当字符为左括号时入栈，当为又括号时，与栈顶比较是否对应
  * @param {string} s
  * @return {boolean}
  */
@@ -24,4 +25,48 @@ var isValid = function(s) {
   }else{
     return false
   }
+};
+
+var isValid = function(s) {
+  var myStack = [];
+  var result = true;
+  for (var i =0; i < s.length; i++) {
+    var popped;
+    switch(s[i]) {
+      case '{':
+        myStack.push(s[i]);
+        break;
+      case '[':
+        myStack.push(s[i]);
+        break;
+      case '(':
+        myStack.push(s[i]);
+        break;
+      case '}':
+        popped = myStack.pop();
+        if (popped !== '{') {
+            result = false;
+            return result;
+        } 
+        break;
+      case ']':
+        popped = myStack.pop();
+        if (popped !== '[') {
+            result = false;
+            return result;
+        } 
+        break;
+      case ')':
+        popped = myStack.pop();
+        if (popped !== '(') {
+            result = false;
+            return result;
+        } 
+        break; 
+    }
+  }
+  if (myStack.length> 0) {
+      result = false;
+  } 
+  return result;
 };
