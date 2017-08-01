7afe77e429c881ec661c5ab25fb54a4c079ef274
sihai <464990030@qq.com>
2017-08-01 14:08:58 +0800

    leetcode No26 done

--- leetcode/26-remove-duplicates-from-sorted-array.js
+++ leetcode/26-remove-duplicates-from-sorted-array.js
@@ -1,7 +1,39 @@
 /**
+ * 给定排序好的数组，返回去重后的长度
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0
+  if (nums.length === 1) return 1
+
+  var len = nums.length - 1
+  var result = 0
+  for(var i = len; i >= 0; i--){
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1)
+    }else{
+      result += 1
+    }
+  }
+
+  return result
+};
+// 参考答案
+var removeDuplicates = function(nums) {
+    
+  var i = 0;
+  var j = 0;
   
+  while(j < nums.length) {
+      if (nums[i] == nums[j]) {
+          j++
+      } else {
+          i++;
+          nums[i] = nums[j];
+          j++;
+      }
+  }
+  
+  return i + 1;
 };
