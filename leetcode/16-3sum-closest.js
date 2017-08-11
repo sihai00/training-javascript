7174336e0a0fe110c56dcbb3795e0670d7bc0919
sihai <464990030@qq.com>
2017-08-11 10:00:28 +0800

    leetcode No16 done

--- leetcode/16-3sum-closest.js
+++ leetcode/16-3sum-closest.js
@@ -1,22 +1,25 @@
 /**
+ * 题目：给定一组数组和目标值，计算数组中三个数的和使得最接近target值
+ * 思路：跟15思路异曲同工
+      1.排序
+      2.假定一个值，然后循环从两端取值相加，依次和target比较，如果比target大，那么left+1，如果小right-1，相等则返回
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
   if (nums.length === 0) return []
-  if (nums.length === 3) return nums[0] + nums[1] + nums[2]
 
   nums.sort((a, b) => a - b)
-  var result = 0
-  var diff = 0
+  var result = (nums[0] + nums[1] + nums[2])
 
   for (var i = 0; i < nums.length - 2; i++) {
     var start = i + 1
     var end = nums.length - 1
-    var sum = nums[i] + nums[start] + nums[end]
 
-    while(start + 1 < end){
+    while(start < end){
+      sum = nums[i] + nums[start] + nums[end]
+
       if (sum === target) {
         return target
       }else if(sum < target){
@@ -24,16 +27,9 @@ var threeSumClosest = function(nums, target) {
       }else{
         end -= 1
       }
-      sum = nums[i] + nums[start] + nums[end]
-    }
 
-    if (diff === 0){
-      result = sum
-      diff = Math.abs(target - sum)
-    }else{
-      if (Math.abs(target - sum) <= diff) {
+      if (Math.abs(target - sum) < Math.abs(target - result)) {
         result = sum
-        diff = Math.abs(target - sum)
       }
     }
   }
