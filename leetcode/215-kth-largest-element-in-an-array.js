28012ae4c906f5c1d01c1ebd359e0ef3e414f38e
sihai <464990030@qq.com>
2017-12-09 23:52:00 +0800

    lc 215 done

--- leetcode/215-kth-largest-element-in-an-array.js
+++ leetcode/215-kth-largest-element-in-an-array.js
@@ -4,5 +4,39 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    
+  if (nums.length <= 0) return 
+  let arr = nums
+  let len = arr.length
+  
+  while(len > 0){
+    if (len === 1) return arr[0]
+
+    let first = arr[0]
+    let max = []
+    let min = []
+    let mid = []
+
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === first) {
+        mid.push(arr[i])
+      }else if(arr[i] > first){
+        max.push(arr[i])
+      }else if(arr[i] < first){
+        min.push(arr[i])
+      }
+    }
+
+    if (k <= max.length) {
+      arr = max
+    }else{
+      if (k > mid.length + max.length) {
+        arr = min
+        k -= (max.length + mid.length)
+      }else{
+        return mid[0]
+      }
+    }
+
+    len = arr.length
+  }
 };
