2dae99f6bb928530514188f59577eebc8495a312
sihai00 <464990030@qq.com>
2017-12-28 00:09:41 +0800

    leetcode 49 done

--- leetcode/49-group-anagrams.js
+++ leetcode/49-group-anagrams.js
@@ -1,7 +1,24 @@
 /**
+ * 给定数组，按包含相同字母分组
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-  
+  // 使用obj的键记录包含字母的子元素，值记录该子元素在数组中的位置
+  var obj = {}
+  var res = []
+
+  strs.forEach(v => {
+    let str = v.split('').sort().join('')
+
+    if (obj[str] === undefined) {
+      obj[str] = res.length
+      res.push([v])
+    }else{
+      res[obj[str]].push(v)
+    }
+  })
+
+  return res
 };
+
