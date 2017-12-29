9a32244497e2bc804af83be0be6bb3bfada4c26d
sihai00 <464990030@qq.com>
2017-12-29 23:53:42 +0800

    leetcode 447 done

--- leetcode/447-number-of-boomerangs.js
+++ leetcode/447-number-of-boomerangs.js
@@ -1,7 +1,36 @@
 /**
+ * 给出一个平面上的n个点，寻找存在多少个由这些点构成的三元组（i,j,k）,
+ * 使得i,j两点的距离等于i,k两点的距离。其中n最多为500，且所有的点坐标范围在[-10000, 10000]之间
+ * 例如：[[0,0], [1,0], [2,0]]，则结果为2
+ * 两个结果为[[1,0],[0,0],[2,0]]和[[1,0],[2,0],[0,0]]
+ *
  * @param {number[][]} points
  * @return {number}
  */
 var numberOfBoomerangs = function(points) {
-  
+  var res = 0
+
+  for (var i = 0; i < points.length; i++) {
+    let map = new Map()
+    for (var j = 0; j < points.length; j++) {
+      if (i !== j) {
+        let dis = getDis(i, j)
+        let count = ~~map.get(dis)
+        map.set(dis, count + 1)
+      }
+    }
+
+    Object.keys(map).forEach(v => {
+      if (v > 1) {
+        res += map.get(v) * (map.get(v) - 1)
+      }
+    })
+  }
+
+  return res
+
+  function getDis(i, j){
+    return (points[i][0] - points[j][0]) * (points[i][0] - points[j][0]) + 
+    (points[i][1] - points[j][1]) * (points[i][1] - points[j][1])
+  }
 };
