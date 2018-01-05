6c4d54f1c6da14547047da0c9f90240bb058ed7f
sihai00 <464990030@qq.com>
2018-01-06 00:38:35 +0800

    leetcode 83 done

--- leetcode/83-remove-duplicates-from-sorted-list.js
+++ leetcode/83-remove-duplicates-from-sorted-list.js
@@ -6,21 +6,22 @@
  * }
  */
 /**
+ * 给定一个有序链表，删除其中所有重复元素，使得每个元素只保留一次
+ * 如：1->1->2，返回1->2
  * @param {ListNode} head
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-  var map = new Map()
+  if (head == null) return null
   var cur = head
 
-  while(cur !== null){
-    let pre = cur.next
+  while(cur.next !== null){
+    let next = cur.next
 
-    if (!map.get(res)) {
-      map.set(res, 1)
-      cur = pre
+    if (next.val !== cur.val) {
+      cur = next
     }else{
-      cur = pre.next
+      cur.next = next.next
     }
   }
 
