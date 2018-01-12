a774dd92c616c1983b0e3cb338fb51acc4885c9b
sihai00 <464990030@qq.com>
2018-01-13 00:45:50 +0800

    leetcode 82 done

--- leetcode/82-remove-duplicates-from-sorted-list-ii.js
+++ leetcode/82-remove-duplicates-from-sorted-list-ii.js
@@ -7,6 +7,7 @@
  */
 /**
  * 给定一个有序列表，将其中有重复的元素全部删除
+ * 思路：在循环内循环判断当前节点与下一个节点是否相等
  * @param {ListNode} head
  * @return {ListNode}
  */
@@ -14,7 +15,19 @@ var deleteDuplicates = function(head) {
   var cur = new ListNode(0), curIndex = cur
   cur.next = head
 
-  while(cur.next !== null){
-    
+  while(cur.next){
+    let curnext = cur.next
+
+    while(curnext.next && curnext.val === curnext.next.val){
+      curnext = curnext.next
+    }
+
+    if (curnext !== cur.next) {
+      cur.next = curnext.next;
+    }else{
+      cur = cur.next
+    }
   }
+
+  return curIndex.next
 };
