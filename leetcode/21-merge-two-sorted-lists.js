463836cddb93f9f939f124bbfe367cf9020201f6
sihai00 <464990030@qq.com>
2018-01-13 23:41:13 +0800

    leetcode 21 done

--- leetcode/21-merge-two-sorted-lists.js
+++ leetcode/21-merge-two-sorted-lists.js
@@ -6,6 +6,7 @@
  * }
  */
 /**
+ * 归并两个有序的链表
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
@@ -33,4 +34,33 @@ var mergeTwoLists = function(l1, l2) {
     result[i].next = result[i + 1]
   }
   return result[0]
+};
+
+var mergeTwoLists = function(l1, l2) {
+  var left = new ListNode(-1), leftIndex = left
+  var right = new ListNode(-1), rightIndex = right
+  var cur = new ListNode(-1), curIndex = cur
+
+  left.next = l1
+  right.next = l2
+
+  while(left.next && right.next){
+    if (left.next.val < right.next.val) {
+      cur.next = new ListNode(left.next.val)
+      left = left.next
+    }else{
+      cur.next = new ListNode(right.next.val)
+      right = right.next
+    }
+
+    cur = cur.next
+  }
+
+  if (left.next) {
+    cur.next = left.next
+  }else{
+    cur.next = right.next
+  }
+
+  return curIndex.next
 };
