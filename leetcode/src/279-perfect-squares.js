250e219ba566234fede8f95c9109b46f7305ee9e
sihai00 <464990030@qq.com>
2018-02-06 23:50:19 +0800

    leetcode 279 done

--- leetcode/src/279-perfect-squares.js
+++ leetcode/src/279-perfect-squares.js
@@ -1,4 +1,5 @@
 /**
+ * 题意：给出一个正整数n，寻找最少的完全平方数，使他们的和为n。
  * @param {number} n
  * @return {number}
  */
@@ -17,4 +18,18 @@ var numSquares = function(n) {
     }
   }
   return dp[n]
+}
+
+function numSquares(n){
+  if (n === 0) {
+    return 0;
+  }
+  let res = [0];
+  for (var i = 1; i <= n; i++) {
+    res.push(100);
+    for (var j = 1; j * j <= i; j++) {
+      res[i] = Math.min(res[i - j * j] + 1, res[i]);
+    }
+  }
+  return res[res.length - 1];
 }
