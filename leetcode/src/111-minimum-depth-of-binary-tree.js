e5a90d06a9427e829ab033f123d72dbab66c1ac9
sihai00 <464990030@qq.com>
2018-02-12 09:16:16 +0800

    leetcode 111 done

--- leetcode/src/111-minimum-depth-of-binary-tree.js
+++ leetcode/src/111-minimum-depth-of-binary-tree.js
@@ -6,9 +6,21 @@
  * }
  */
 /**
+ * 求一颗二叉树的最低深度
  * @param {TreeNode} root
  * @return {number}
  */
 var minDepth = function(root) {
+  if(root === null){
+    return 0;
+  }
   
+  if(root.left === null){
+    return minDepth(root.right) + 1;
+  }
+  if(root.right === null){
+    return minDepth(root.left) + 1;
+  }
+  
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
