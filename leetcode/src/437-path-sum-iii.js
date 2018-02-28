faa9e0c533451bfb544bdef7ec55dc1778d0728d
sihai00 <464990030@qq.com>
2018-02-28 10:48:23 +0800

    leetcode 437 done

--- leetcode/src/437-path-sum-iii.js
+++ leetcode/src/437-path-sum-iii.js
@@ -13,5 +13,28 @@
  * @return {number}
  */
 var pathSum = function(root, sum) {
+  if (!root) return 0
   
+  // 包含当前节点的路径
+  var res = findPath(root, sum)
+
+  // 不包含当前节点的路径
+  res += pathSum(root.left, sum)
+  res += pathSum(root.right, sum)
+
+  return res
 };
+
+var findPath = function(root, sum){
+  if (!root) return 0
+
+  var res = 0
+  if (root.val === sum) res += 1
+
+  res += findPath(root.left, sum - root.val)
+  res += findPath(root.right, sum - root.val)
+
+  return res
+};
+
+
