e84ee9331eb7f82390eeb6354c3d67194b023373
sihai00 <464990030@qq.com>
2018-03-04 22:27:24 +0800

    leetcode 98 done

--- leetcode/src/98-validate-binary-search-tree.js
+++ leetcode/src/98-validate-binary-search-tree.js
@@ -6,10 +6,19 @@
  * }
  */
 /**
- * 题意：给定一颗二叉树，验证起是否为二分搜索树
+ * 题意：给定一颗二叉树，验证其是否为二分搜索树
+ * 二分搜索树：当前节点比左孩子大，比右孩子小
  * @param {TreeNode} root
  * @return {boolean}
  */
 var isValidBST = function(root) {
-  
+  return isBST(root, -Infinity, Infinity);
 };
+
+function isBST(root, min, max) {
+  if(root === null) return true; 
+  
+  if(root.val <= min || root.val >= max) return false;
+  
+  return isBST(root.left, min, root.val) && isBST(root.right, root.val, max);
+}
